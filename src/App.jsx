7d274d8311a986a5765127fbd9b163e072b1f70c
ashbaArshad <ashbaarshad777@gmail.com>
2024-02-06 17:14:45 +0500

    Header Componenet Added

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import About from './Pages/About';
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
 import Profile from './Pages/Profile';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element = {<Home/>}></Route>
         <Route path='/about' element = {<About/>}></Route>
@@ -15,6 +17,7 @@ export default function App() {
         <Route path='/sign-up' element = {<SignUp/>}></Route>
         <Route path='/profile' element = {<Profile/>}></Route>
       </Routes>    
+      
     </BrowserRouter>
   )
 }
